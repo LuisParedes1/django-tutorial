db6439934f419fc37b7bd38c06aeb71aca4c0ffd
Luis Paredes <lparedesr@fi.uba.ar>
2025-07-13 15:50:51 -0300

    first migration

--- polls/models.py
+++ polls/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
 class Question(models.Model):
-    pass
+    question = models.CharField(max_length=200)
+    publication_date = models.DateTimeField("date published")
 
-
-class Question(models.Model):
-    pass
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200) 
+    votes = models.IntegerField(default=0)
