a8aab3f1cb76731840c199c20ef52f54cfb66e7e
Luis Paredes <lparedesr@fi.uba.ar>
2025-07-14 17:02:42 -0300

    refactor

--- polls/models.py
+++ polls/models.py
@@ -1,8 +1,8 @@
+import datetime
 from django.db import models
 from datetime import datetime
 from django.utils import timezone
 
-# Create your models here.
 class Question(models.Model):
     question = models.CharField(max_length=200)
     publication_date = models.DateTimeField("date published")
--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpRequest, Http404
-from polls.models import Question
+from django.http import HttpResponse, HttpRequest, Http404, HttpResponseRedirect
+from polls.models import Question, Choice
+from django.db.models import F
 from django.template import loader
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 
 # Using a template
 def index(request: HttpRequest) -> HttpResponse:
@@ -38,13 +40,32 @@ def detail_with_error_handling_and_shortcut(request: HttpRequest, question_id: i
     question = get_object_or_404(Question, pk=question_id)
     # There’s also a get_list_or_404() function, which works just as get_object_or_404() – except using filter() instead of get(). 
     # It raises Http404 if the list is empty.
-    return render(request, "polls/detail.html", {"question": ques})
+    return render(request, "polls/detail.html", {"question": question})
 
 
 def results(request: HttpRequest, question_id: int):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
 
 
 def vote(request: HttpRequest, question_id: int):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice.",
+            },
+        )
+    else:
+        selected_choice.votes = F("votes") + 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
